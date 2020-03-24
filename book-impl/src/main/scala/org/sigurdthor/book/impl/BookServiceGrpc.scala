04e615bbe02a0135e6a30be5fc77bf012bc4dbab
Yaroslav Yaremenko <yaroslav.yaremenko@leonteq.com>
2020-03-24 18:29:26 +0200

    error logging improved

--- book-impl/src/main/scala/org/sigurdthor/book/impl/BookServiceGrpc.scala
+++ book-impl/src/main/scala/org/sigurdthor/book/impl/BookServiceGrpc.scala
@@ -3,7 +3,6 @@ package org.sigurdthor.book.impl
 import java.time.OffsetDateTime
 
 import com.lightbend.lagom.scaladsl.persistence.PersistentEntityRegistry
-import com.typesafe.scalalogging.StrictLogging
 import io.grpc.Status
 import izumi.logstage.api.IzLogger
 import izumi.logstage.api.Log.Level.Trace
@@ -13,16 +12,18 @@ import org.sigurdthor.book.api.domain.model._
 import org.sigurdthor.book.domain.BookEntity
 import org.sigurdthor.book.domain.commands.{AddBook, GetBook}
 import org.sigurdthor.book.domain.validation.Validator
+import org.sigurdthor.book.lib.ErrorLoggers._
+import org.sigurdthor.book.lib.Transformations._
 import org.sigurdthor.bookshelf.grpc.bookservice.ZioBookservice.BookService
-import org.sigurdthor.bookshelf.grpc.bookservice.{AddBookRequest, AddBookResponse, BookResponse, GetBookRequest, ZioBookservice}
+import org.sigurdthor.bookshelf.grpc.bookservice._
 import zio.clock.Clock
 import zio.{Has, IO, ZLayer}
-import org.sigurdthor.book.lib.Transformations._
 
-import scala.concurrent.Future
+import scala.concurrent.ExecutionContext
 
 
-class BookServiceGrpc(persistentEntityRegistry: PersistentEntityRegistry)(implicit validator: Validator[Book]) extends StrictLogging {
+class BookServiceGrpc(persistentEntityRegistry: PersistentEntityRegistry)
+                     (implicit ec: ExecutionContext, validator: Validator[Book]) {
 
   type BookServiceM = Has[ZioBookservice.BookService]
 
@@ -37,11 +38,7 @@ class BookServiceGrpc(persistentEntityRegistry: PersistentEntityRegistry)(implic
           val flow = for {
             book <- validator.validate(req.toBook)
             _ <- IO.fromFuture { implicit ec =>
-              bookEntityRef(book.isbn.value).ask(AddBook(book.title, book.authors, book.description)).recoverWith {
-                case e: Throwable =>
-                  logger.error("addBook command failure", e)
-                  Future.failed(e)
-              }
+              bookEntityRef(book.isbn.value).ask(AddBook(book.title, book.authors, book.description)).logError
             } *> log.debug(s"Book ${req.isbn} has been added")
           } yield AddBookResponse(OffsetDateTime.now().toString)
 
@@ -50,11 +47,7 @@ class BookServiceGrpc(persistentEntityRegistry: PersistentEntityRegistry)(implic
 
         def getBook(req: GetBookRequest): IO[Status, BookResponse] =
           log.debug(s"getBook Request $req") *> IO.fromFuture { implicit ec =>
-            bookEntityRef(req.isbn).ask(GetBook).recoverWith {
-              case e: Throwable =>
-                logger.error("getBook command failure", e)
-                Future.failed(e)
-            }
+            bookEntityRef(req.isbn).ask(GetBook).logError
           }.bimap(
             _ => Status.NOT_FOUND,
             _.toResponse)
---
+++ book-impl/src/main/scala/org/sigurdthor/book/lib/ErrorLoggers.scala
@@ -0,0 +1,19 @@
+package org.sigurdthor.book.lib
+
+import com.typesafe.scalalogging.StrictLogging
+
+import scala.concurrent.{ExecutionContext, Future}
+
+object ErrorLoggers extends StrictLogging {
+
+  implicit class FutureLogger[T](f: Future[T])(implicit ec: ExecutionContext) {
+
+    def logError =
+      f.recoverWith {
+        case e: Throwable =>
+          logger.error("Command failure", e)
+          Future.failed(e)
+      }
+  }
+
+}
